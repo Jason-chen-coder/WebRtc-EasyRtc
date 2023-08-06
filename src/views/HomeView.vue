f5d942815363832bf04699dba4a16951611abc56
chenhongxin <chenhongxin@genomics.cn>
2023-08-06 17:28:19 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <main>
+  选择摄像头 ：
+  <div class="enlarge_img" style="display:none;"><img src="" @load="getImageSize" class="enlarge img img"/></div>
+  </main>
+</template>
+<script>
+export default{
+ async created(){
+    const res = await  navigator.mediaDevices.enumerateDevices();
+    console.log(res,'==========>res')
+  },  
+  methods:{
+  },
+  data() {
+    return{
+      num:1
+    }
+  }
+}
+</script>
